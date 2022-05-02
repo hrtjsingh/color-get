d0e57ec0bcb1a4231797c3487545f176d17480f1
Hartej Singh <hartej@indglobal-consulting.com>
2022-05-02 17:03:03 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect, useRef } from "react";
+import axios from "axios";
+import Masonry from "react-masonry-css";
 
-function App() {
+import "./styles.css";
+
+export default function App() {
+  const ref = useRef();
+  const [results, setResults] = useState(false);
+  const [renderData, setRenderData] = useState(false);
+  const search = async () => {
+    let val = ref.current.value;
+    const res = await axios.get(
+      `https://api.color.pizza/v1/names/?name=${val}`
+    );
+    let allData = res.data.colors;
+    let mainData = allData.map((color, i) => {
+      return (
+        <div key={i} style={{ background: `${color.hex}` }}>
+          <h2>{color.name}</h2>
+          <h2>{color.hex}</h2>
+        </div>
+      );
+    });
+    setRenderData(
+      <Masonry
+        breakpointCols={3}
+        className="my-masonry-grid"
+        columnClassName="my-masonry-grid_column"
+      >
+        {mainData}
+      </Masonry>
+    );
+    setResults(true);
+  };
+  useEffect(() => {}, [renderData]);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Color a word!</h1>
+      <input placeholder="Enter a word" ref={ref} />
+      <button onClick={() => search()}>Search</button>
+      {results && <div className="results">{renderData}</div>}
     </div>
   );
 }
-
-export default App;
